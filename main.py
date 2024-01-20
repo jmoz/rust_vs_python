9ce033e99ac78a5295f980c76ae898dfdd345376
James Morris <github@jmoz.co.uk>
2024-01-20 12:50:16 +0000

    Add updated python and rust with loop vs threads

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ import polars as pl
 import time
 
 
+def filter_df(csv_df, i):
+    filtered_df = csv_df.filter(
+        pl.col("a").gt(0.2)
+        & pl.col("b").lt(0.8)
+        & pl.col("c").gt(0.5)
+        & pl.col("d").lt(0.5)
+        & pl.col("e").ne(i)  # additional non equality check, value from first row csv
+    )
+    return filtered_df
+
+
+def calculate_df(filtered_df, i):
+    # Mimic some euclidean distance type calculation
+    calculated_df = filtered_df.select(
+        ((pl.col("a") / pl.col("a").max()) / (i / pl.col("a").max())).pow(2).sqrt()
+    )
+    return calculate_df
+
+
 def main():
     start_time = time.perf_counter() * 1000
 
-    csv_file = os.getenv("CSV_FILE")
+    csv_df = pl.read_csv(os.getenv("CSV_FILE"))
 
-    csv_df = pl.read_csv(csv_file)
-    print(f"Csv read in {time.perf_counter() * 1000 - start_time}ms")
-
-    print(f"Loaded df {csv_df}")
-    print(f"Df describe {csv_df.describe()}")
-
-    filtered_df = csv_df.filter(
-        pl.col("a").gt(0.2) &
-        pl.col("b").lt(0.8) &
-        pl.col("c").gt(0.5) &
-        pl.col("d").lt(0.5) &
-        pl.col("e").ne(0.5182602093634714) # additional non equality check, value from first row csv
-    )
-    print(f"Filtered df {filtered_df}")
-
-    # Mimic some euclidean distance type calculation
-    calculated_df = filtered_df.select(
-        (
-            (pl.col("a") / pl.col("a").max()) / (0.5 / pl.col("a").max())
-        ).pow(2).sqrt()
-    )
-    print(f"Calculated df {calculated_df}")
+    # some standard looping to mimic running dataframe calcs on multiple items
+    LOOP_SIZE = 1000
+    for i in range(0, LOOP_SIZE):
+        scaled_i = i / LOOP_SIZE
+        filtered_df = filter_df(csv_df, scaled_i)
+        calculated_df = calculate_df(filtered_df, scaled_i)
 
     print(f"Finished in {(time.perf_counter() * 1000 - start_time)}ms")
 
--- main.rs
+++ main.rs
@@ -1,38 +1,56 @@
 use polars::prelude::*;
 use std::time::Instant;
+use threadpool::ThreadPool;
 
 
-fn main() {
-    let start_time = Instant::now();
-
-    let csv_file = std::env::var("CSV_FILE").expect("Set env CSV_FILE error");
-
-    let csv_df = CsvReader::from_path(csv_file).expect("Could not load csv error").has_header(true).finish().expect("Finish error");
-    println!("Csv read in {}ms", start_time.elapsed().as_millis());
-
-    // cargo add polars -F fmt,describe
-    println!("Loaded df {}", csv_df);
-    println!("Df describe {}", csv_df.describe(None).expect("Describe error"));
-
+fn filter_df(csv_df: &DataFrame, i: &f64) -> LazyFrame {
     // Filter on multiple columns
     let filtered_df = csv_df.clone().lazy().filter(
         col("a").gt(0.2)
         .and(col("b").lt(0.8))
         .and(col("c").gt(0.5))
         .and(col("d").lt(0.5))
-        .and(col("e").neq(0.5182602093634714)) // additional non equality check, value from first row csv
-    ).collect().expect("Error filtering");
-    println!("Filtered df {}", filtered_df);
+        .and(col("e").neq(*i)) // additional non equality check, value from first row csv
+    );
+    filtered_df
+}
 
+fn calculate_df(filtered_df: LazyFrame, i: &f64) -> DataFrame {
     // Mimic some euclidean distance type calculation
-    let calculated_df = filtered_df.clone().lazy().select(
+    let calculated_df = filtered_df.select(
         [
             (
-                (col("a") / col("a").max()) / (lit(0.5) / col("a").max())
+                (col("a") / col("a").max()) / (lit(*i) / col("a").max())
             ).pow(2).sqrt()
         ]
-    ).collect().expect("Error select");
-    println!("Calculated df {}", calculated_df);
+    ).collect().unwrap();
+    calculated_df
+}
+
+fn main() {
+    let start_time = Instant::now();
+
+    let csv_file = std::env::var("CSV_FILE").expect("Set env CSV_FILE error");
+
+    let csv_df = CsvReader::from_path(csv_file).unwrap().finish().unwrap();
+    let csv_df = Arc::new(csv_df);
+
+    const LOOP_SIZE: i16 = 1000;
+    const NUM_THREADS: usize = 8;
+
+    let pool = ThreadPool::new(NUM_THREADS);
+
+    for i in 0..LOOP_SIZE {
+        let csv_df = Arc::clone(&csv_df);
+
+        pool.execute(move || {
+            let scaled_i = f64::from(i) / f64::from(LOOP_SIZE);
+            let filtered_df = filter_df(&csv_df, &scaled_i);
+            let _calculated_df = calculate_df(filtered_df, &scaled_i);
+        });
+    };
+
+    pool.join();
 
     println!("Finished in {}ms", start_time.elapsed().as_millis());
 }
